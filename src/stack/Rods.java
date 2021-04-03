6dd87572202980d4ec4b81d866c87f927fa33ab6
jayjay <jinwoolee131@gmail.com>
2021-04-03 14:13:37 +0900

    백준 스택 #10799 쇠막대기

---
+++ src/stack/Rods.java
@@ -0,0 +1,55 @@
+package stack;
+
+import java.util.Stack;
+import java.util.Scanner;
+
+public class Rods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        String arrangement = sc.nextLine();
+        sc.close();
+		System.out.println(solution(arrangement));
+	}
+    public static int solution(String arrangement) {
+        int answer = 0;
+        if(arrangement==null) return answer;
+        Stack <Character> stack = new Stack<Character>();
+        boolean cut=false;
+        char current;
+        for (int i=0, cnt=0;i<arrangement.length();i++) {
+        	current = arrangement.charAt(i);
+        	if(!stack.isEmpty()&&current==')') {
+        		stack.pop();
+        		if(!cut) {
+	        		cut = true;
+	        		cnt-=1;
+	        		answer+=cnt;
+//	        		System.out.println("current => "+ current);
+//	        		System.out.println("cnt => " + cnt);
+//	        		System.out.println("answer => "+ answer);
+//	        		System.out.println("");
+        		}
+        		else {
+	        		cnt-=1;
+	        		answer+=1;
+//	        		System.out.println("current => "+ current);
+//	        		System.out.println("cnt => " + cnt);
+//	        		System.out.println("answer => "+ answer);
+//	        		System.out.println("");
+        		}
+
+        	}
+        	else {
+        		stack.add(current);
+        		cnt+=1;
+        		cut = false;
+//        		System.out.println("current => "+ current);
+//        		System.out.println("cnt => " + cnt);
+//        		System.out.println("");
+        	}
+        }
+        return answer;
+    }
+}
