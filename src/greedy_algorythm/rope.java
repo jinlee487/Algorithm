8a9798f718c6bc1750ebd7eb910629561f3dff3e
jayjay <jinwoolee131@gmail.com>
2021-04-19 17:44:30 +0900

    백준 2217 로프 문제

--- src/greedy_algorythm/rope.java
+++ src/greedy_algorythm/rope.java
@@ -5,7 +5,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-
+/*
+ * SOLVED!!
+ * https://www.acmicpc.net/problem/2217 
+ * 로프
+ * 
+*/
 public class rope {
     public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
