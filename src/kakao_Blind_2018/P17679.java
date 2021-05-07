b4526031d28198487bb0ccaa834b568ee9b68951
jayjay <jinwoolee131@gmail.com>
2021-05-07 14:53:15 +0900

    프로그래머스 17679 프렌즈4블록 문제

---
+++ src/kakao_Blind_2018/P17679.java
@@ -0,0 +1,19 @@
+package kakao_Blind_2018;
+import java.util.*;
+/* 
+Solved! 
+https://programmers.co.kr/learn/courses/30/lessons/17679
+프렌즈4블록
+*/
+public class P17679 {
+    public static void main(String[] args) throws Exception {
+        int m = 4;
+        int n = 5;
+        String[] board = {"CCBDE", "AAADE", "AAABF", "CCBBF"};
+        System.out.println(solution(m,n,board));
+    }
+    public static int solution(int m, int n, String[] board) {
+        int answer = 0;
+        return answer;
+    }
+}
