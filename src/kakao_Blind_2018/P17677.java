1393348271f3b3a7796bc3f9974b508642384cb7
jayjay <jinwoolee131@gmail.com>
2021-05-07 13:00:54 +0900

    프로그래머스 17677 뉴스 클러스터링 문제

---
+++ src/kakao_Blind_2018/P17677.java
@@ -0,0 +1,13 @@
+package kakao_Blind_2018;
+
+public class P17677 {
+    public static void main(String[] arg) throws Exception {
+        String str1 = "FRANCE";
+        String str2 = "french";
+        System.out.println(solution(str1,str2));
+    }
+    public static int solution(String str1, String str2) {
+        int answer = 0;
+        return answer;
+    }
+}
