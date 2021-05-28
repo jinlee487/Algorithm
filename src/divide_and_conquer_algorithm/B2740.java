f8dc9cb82c9fd11dd0a03b07a305108700daba4f
jayjay <jinwoolee131@gmail.com>
2021-05-29 00:47:14 +0900

    백준 2740 행렬 곱셈 문제

--- src/divide_and_conquer_algorithm/B2740.java
+++ src/divide_and_conquer_algorithm/B2740.java
@@ -27,14 +27,12 @@ class B2740 {
             arrB[i] = br.readLine().split(" ");
         }
         StringBuilder sb = new StringBuilder();	
-		// 행렬 계산 및 출력
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < K; j++) {
 				int sum = 0;
 				for(int k = 0; k < M; k++) {
 					sum += Integer.parseInt(arrA[i][k]) * Integer.parseInt(arrB[k][j]);
 				}
-				// A의 i행의 j열 연산이 끝나면 바로 출력문으로 보내준다. 
 				sb.append(sum).append(' ');
 			}
 			sb.append('\n');
