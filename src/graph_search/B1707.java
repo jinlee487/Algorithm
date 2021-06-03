d7b9c1ad0366110beb0b7e707ed509f2e9f8f93f
jayjay <jinwoolee131@gmail.com>
2021-06-03 23:21:55 +0900

    백준 1707 이분그래프 문제

---
+++ src/graph_search/B1707.java
@@ -0,0 +1,25 @@
+package graph_search;
+import java.io.*;
+import java.util.*;
+
+class B1707 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out;
+        int K = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int V, E;
+        for(int i=0;i<K;i++){
+            st = new StringTokenizer(br.readLine());
+            V = Integer.parseInt(st.nextToken());
+            E = Integer.parseInt(st.nextToken());
+            
+        }
+
+
+
+
+
+    }
+    
+}
