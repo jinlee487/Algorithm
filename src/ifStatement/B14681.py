e59f6408d9239929bbaa8c6c644c963b069c098e
jayjay <jinwoolee131@gmail.com>
2021-06-22 09:54:12 +0900

    백준 14681 사분면 고르기 문제

---
+++ src/ifStatement/B14681.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/14681
+# 사분면 고르기
+# (−1000 ≤ x ≤ 1000; x ≠ 0)
+# (−1000 ≤ y ≤ 1000; y ≠ 0)
+
+x = int(input())
+y = int(input())
+if x>0 and y >0:
+    print(1)
+elif x<0 and y>0:
+    print(2)
+elif x<0 and y<0:
+    print(3)
+elif x>0 and y<0:
+    print(4)
