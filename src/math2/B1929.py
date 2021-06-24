45f27186788b6521e320618c7ff73c350f564012
jayjay <jinwoolee131@gmail.com>
2021-06-24 11:00:24 +0900

    백준 1929 소수 구하기 문제

--- src/math2/B1929.py
+++ src/math2/B1929.py
@@ -3,17 +3,15 @@
 
 M,N = map(int,input().split(" "))
 
-check = [False for i in range(N+1)]
-for i in range(2, int(N**0.6)):
-    if check[i] == False:
-        for j in range(2 * i, N + 1, i):
-            check[j] = True
-l = []
-for i in range(M, N + 1):
-    if i >= 2:
-        if check[i] == False:
-            l.append(i)
+r = N + 1
 
-if len(l) != 0:
-    for i in l:
+check = [False for _ in range(r)]
+for i in range( 2, int(r**0.8) ) : # 2부터 ~ 범위(r)의 제곱근까지
+    if check[i] == False : # 지워지지 않고, 가장 작은수라면
+        for j in range( i*2 , r, i ): # i의 2배수 부터, 3배수, 4배수, .... 범위끝까지 지운다.(True로)
+            check[j] = True
+prime_number = [i for i,j in enumerate(check) if i>=2 and j==False]
+
+for i in prime_number:
+    if N >= i >= M:
         print(i)
